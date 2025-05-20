5b939ec70aae21f32f46f0545ff1e787dfde5ed8
Dan Maxim <danemaxim@gmail.com>
2025-05-20 15:56:19 -0400

    Add react app to app host

--- src/MxInfo.DeviceManager/MxInfo.DeviceManager.AppHost/Program.cs
+++ src/MxInfo.DeviceManager/MxInfo.DeviceManager.AppHost/Program.cs
@@ -1,6 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.MxInfo_DeviceManager_Api>("deviceManagerApi");
+var deviceManagerApi = builder.AddProject<Projects.MxInfo_DeviceManager_Api>("deviceManagerApi");
     
-    
+builder.AddNpmApp("react", "../MxInfo.DeviceManager.UI", "start")
+    .WithReference(deviceManagerApi)
+    .WaitFor(deviceManagerApi)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
 builder.Build().Run();
